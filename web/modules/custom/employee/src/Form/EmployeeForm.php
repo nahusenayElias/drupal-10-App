d55b6383722284995607b0b2fea3c0a3efa8bf1a
Elias Hagos <eliasars@yahoo.com>
2024-10-15 01:54:59 +0300

    Database connection created

--- web/modules/custom/employee/src/Form/EmployeeForm.php
+++ web/modules/custom/employee/src/Form/EmployeeForm.php
@@ -6,6 +6,7 @@ namespace Drupal\employee\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Database;
 
 /**
  * Provides a employee form.
@@ -104,6 +105,18 @@ final class EmployeeForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $conn = Database::getConnection();
+
+    $formField = $form_state->getValues();
+
+    $formData['emp_firstname'] = $formField['emp_firstname'];
+    $formData['emp_lastname'] = $formField['emp_lastname'];
+    $formData['emp_email'] = $formField['emp_email'];
+    $formData['emp_zipcode'] = $formField['emp_zipcode'];
+
+    $conn->insert('employee')
+      ->fields($formData)->execute();
+
     $this->messenger()->addStatus($this->t('Employee data has been saved successfully.'));
     $form_state->setRedirect('employee.employee');
   }
